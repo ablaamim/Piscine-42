5818d663ad306d98e08782c822f3fdafc89843df
alaamimi <aku-no-hana@outlook.fr>
2020-10-21 00:07:12 +0100

    First recursive function

--- Piscine_C/DAY02/ex06/ft_putnbr.c
+++ Piscine_C/DAY02/ex06/ft_putnbr.c
@@ -1,3 +1,32 @@
+/*-----------------------------------------------------
+-                               __                    -
+-                             .d$$b                   -
+-                           .' TO$;\                  -
+-                          /  : TP._;                 -
+-                         / _.;  :Tb|                 -
+-                        /   /   ;j$j                 -
+-                    _.-"       d$$$$                 -
+-                  .' ..       d$$$$;                 -
+-                 /  /P'      d$$$$P. |\              -
+-                /   "      .d$$$P' |\^"l             -
+-              .'           `T$P^"""""  :             -
+-          ._.'      _.'                ;             -
+-       `-.-".-'-' ._.       _.-"    .-"              -
+-     `.-" _____  ._              .-"                 -
+-    -(.g$$$$$$$b.              .'                    -
+-      ""^^T$$$P^)            .(:                     -
+-        _/  -"  /.'         /:/;                     -
+-     ._.'-'`-'  ")/         /;/;                     -
+------------------------------------------------------*/
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1 , &c, 1);
+}
+
 void	ft_putnbr(int nb)
 {
 	long number;
@@ -21,4 +50,17 @@ void	ft_putnbr(int nb)
 	{
 		ft_putchar(number + '0');
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_putnbr(12345);
+	ft_putchar('\n');
+	ft_putnbr(-122);
+	ft_putchar('\n');
+	ft_putnbr(2147483647);
+	ft_putchar('\n');
+	return (EXIT_SUCCESS);
 }
