b718d051ebac00aa8039f0b6294af13b3fe3e3bb
alaamimi <aku-no-hana@outlook.fr>
2020-10-20 23:09:27 +0100

    Slight modification

--- Piscine_C/DAY02/ex01/ft_print_reverse_alphabet.c
+++ Piscine_C/DAY02/ex01/ft_print_reverse_alphabet.c
@@ -1,11 +1,49 @@
-void	ft_print_reverse_alphabet(void)
-{
-	char letter;
+/*-----------------------------------------------------
+-                               __                    -
+-                             .d$$b                   -
+-                           .' TO$;\                  -
+-                          /  : TP._;                 -
+-                         / _.;  :Tb|                 -
+-                        /   /   ;j$j                 -
+-                    _.-"       d$$$$                 -
+-                  .' ..       d$$$$;                 -
+-                 /  /P'      d$$$$P. |\              -
+-                /   "      .d$$$P' |\^"l             -
+-              .'           `T$P^"""""  :             -
+-          ._.'      _.'                ;             -
+-       `-.-".-'-' ._.       _.-"    .-"              -
+-     `.-" _____  ._              .-"                 -
+-    -(.g$$$$$$$b.              .'                    -
+-      ""^^T$$$P^)            .(:                     -
+-        _/  -"  /.'         /:/;                     -
+-     ._.'-'`-'  ")/         /;/;                     -
+------------------------------------------------------*/
+/* The first exercice is quite similar to this one, the only difference
+ is the inversed variables values, and using decrementation*/
+#include <unistd.h>
+#include <stdlib.h>
 
-	letter = 'z';
-	while (letter >= 'a')
+void	ft_putchar(char c)
+{
+	write(1 , &c, 1);
+}
+
+void	ft_print_reverse_alphabet()
+{
+	char alph;
+
+	alph = 122; /* man ascii*/
+	while (alph >= 97) 
 	{
-		ft_putchar(letter);
-		letter--;
+		ft_putchar(alph);
+		alph--; /*decrementation*/
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_print_reverse_alphabet();
+	return (EXIT_SUCCESS);
 }
