a5475c93b359e45673bff9ca0a917524cedbc529
alaamimi <aku-no-hana@outlook.fr>
2020-10-20 23:33:02 +0100

    if_else_statements

--- Piscine_C/DAY02/ex03/ft_is_negative.c
+++ Piscine_C/DAY02/ex03/ft_is_negative.c
@@ -1,7 +1,47 @@
+/*-----------------------------------------------------
+-                               __                    -
+-                             .d$$b                   -
+-                           .' TO$;\                  -
+-                          /  : TP._;                 -
+-                         / _.;  :Tb|                 -
+-                        /   /   ;j$j                 -
+-                    _.-"       d$$$$                 -
+-                  .' ..       d$$$$;                 -
+-                 /  /P'      d$$$$P. |\              -
+-                /   "      .d$$$P' |\^"l             -
+-              .'           `T$P^"""""  :             -
+-          ._.'      _.'                ;             -
+-       `-.-".-'-' ._.       _.-"    .-"              -
+-     `.-" _____  ._              .-"                 -
+-    -(.g$$$$$$$b.              .'                    -
+-      ""^^T$$$P^)            .(:                     -
+-        _/  -"  /.'         /:/;                     -
+-     ._.'-'`-'  ")/         /;/;                     -
+------------------------------------------------------*/
+/* This problem is solved easily using the if else statement, also the 
+  ft_putchar function to print in stdout the following value*/
+/* Also a good use of a new type of variables which is an int*/
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1 , &c, 1);
+}
+
 void	ft_is_negative(int n)
 {
 	if (n < 0)
 		ft_putchar('N');
 	else
 		ft_putchar('P');
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_is_negative(42);
+	ft_putchar('\n');
+	return (EXIT_SUCCESS);
 }
