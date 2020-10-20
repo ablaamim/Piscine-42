dccb85d2903f205711006387d7df6f919e04907e
alaamimi <aku-no-hana@outlook.fr>
2020-10-20 23:17:06 +0100

    adjusting

--- Piscine_C/DAY02/ex02/ft_print_numbers.c
+++ Piscine_C/DAY02/ex02/ft_print_numbers.c
@@ -1,11 +1,51 @@
+/*-----------------------------------------------------
+-                               __                    -
+-                             .d$$b                   -
+-                           .' TO$;\                  -
+-                          /  : TP._;                 -
+-                         / _.;  :Tb|                 -
+-                        /   /   ;j$j                 -
+-                    _.-"       d$$$$                 -
+-                  .' ..       d$$$$;                 -
+-                 /  /P'      d$$$$P. |\              -
+-                /   "      .d$$$P' |\^"l             -
+-              .'           `T$P^"""""  :             -
+-          ._.'      _.'                ;             -
+-       `-.-".-'-' ._.       _.-"    .-"              -
+-     `.-" _____  ._              .-"                 -
+-    -(.g$$$$$$$b.              .'                    -
+-      ""^^T$$$P^)            .(:                     -
+-        _/  -"  /.'         /:/;                     -
+-     ._.'-'`-'  ")/         /;/;                     -
+------------------------------------------------------*/
+/* This exercice is quite easy, it needs nothing but 1 instruction which is
+ a while loop, incrementing each value passed on to the putchar function*/
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void	ft_print_numbers(void)
 {
 	char nb;
 
-	nb = '0';
-	while (nb <= '9')
+	nb = 48;
+	while (nb <= 57)
 	{
 		ft_putchar(nb);
 		nb++;
 	}
 }
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_print_numbers();
+	ft_putchar('\n');
+	return (EXIT_SUCCESS);
+}
+
