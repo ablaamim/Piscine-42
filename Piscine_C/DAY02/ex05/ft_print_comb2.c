a4a2da6b5200e5929f35d01e992d98617c1edbc8
alaamimi <aku-no-hana@outlook.fr>
2020-10-21 00:03:21 +0100

    I like this exercice

--- Piscine_C/DAY02/ex05/ft_print_comb2.c
+++ Piscine_C/DAY02/ex05/ft_print_comb2.c
@@ -1,3 +1,32 @@
+/*-----------------------------------------------------
+-                               __                    -
+-                             .d$$b                   -
+-                           .' TO$;\                  -
+-                          /  : TP._;                 -
+-                         / _.;  :Tb|                 -
+-                        /   /   ;j$j                 -
+-                    _.-"       d$$$$                 -
+-                  .' ..       d$$$$;                 -
+-                 /  /P'      d$$$$P. |\              -
+-                /   "      .d$$$P' |\^"l             -
+-              .'           `T$P^"""""  :             -
+-          ._.'      _.'                ;             -
+-       `-.-".-'-' ._.       _.-"    .-"              -
+-     `.-" _____  ._              .-"                 -
+-    -(.g$$$$$$$b.              .'                    -
+-      ""^^T$$$P^)            .(:                     -
+-        _/  -"  /.'         /:/;                     -
+-     ._.'-'`-'  ")/         /;/;                     -
+------------------------------------------------------*/
+/* This function provides a very useful method, which is the div_mod*/
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1 , &c, 1);
+}
+
 void	ft_print_comb2(void)
 {
 	int tab[2];
@@ -8,11 +37,11 @@ void	ft_print_comb2(void)
 		tab[1] = tab[0];
 		while (++tab[1] <= 99)
 		{
-			ft_putchar(tab[0] / 10 + '0');
-			ft_putchar(tab[0] % 10 + '0');
+			ft_putchar(tab[0] / 10 + 48);
+			ft_putchar(tab[0] % 10 + 48);
 			ft_putchar(' ');
-			ft_putchar(tab[1] / 10 + '0');
-			ft_putchar(tab[1] % 10 + '0');
+			ft_putchar(tab[1] / 10 + 48);
+			ft_putchar(tab[1] % 10 + 48);
 			if (tab[0] != 98)
 			{
 			ft_putchar(',');
@@ -20,4 +49,13 @@ void	ft_print_comb2(void)
 			}
 		}
 	}
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_print_comb2();
+	ft_putchar('\n');
+	return (EXIT_SUCCESS);
 }
