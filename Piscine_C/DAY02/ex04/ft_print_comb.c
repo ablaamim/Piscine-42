b300678769e10a6d56150672375d3879b15c64fe
root <root@DESKTOP-5RJJ8PG.localdomain>
2020-10-22 02:39:56 +0100

    Acurrate scii

--- Piscine_C/DAY02/ex04/ft_print_comb.c
+++ Piscine_C/DAY02/ex04/ft_print_comb.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdlib.h>
 
 void	ft_putchar(char c)
 {
@@ -9,22 +10,22 @@ void	ft_print_comb(void)
 {
 	char tab[3];
 
-	tab[0] = '0' - 1;
-	while (tab[0]++ < '7')
+	tab[0] = 47;
+	while (tab[0]++ < 55)
 	{
 		tab[1] = tab[0];
-		while (tab[1]++ < '8')
+		while (tab[1]++ < 56)
 		{
 			tab[2] = tab[1];
-			while (tab[2]++ < '9')
+			while (tab[2]++ < 57)
 			{
 				ft_putchar(tab[0]);
 				ft_putchar(tab[1]);
 				ft_putchar(tab[2]);
-				if (tab[0] != '7')
+				if (tab[0] != 55)
 				{
-				ft_putchar(',');
-				ft_putchar(' ');
+				ft_putchar(44);
+				ft_putchar(32);
 				}
 			}
 		}
@@ -34,5 +35,6 @@ void	ft_print_comb(void)
 int	main(void)
 {
 	ft_print_comb();
+	ft_putchar(10);
 	return (0);
 }
