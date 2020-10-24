0ca5098dcb52c98ccd529aa1907c7b20988be0d3
alaamimi <aku-no-hana@outlook.fr>
2020-10-24 21:10:39 +0200

    DAY03

---
+++ Piscine_C/DAY03/ex07/ft_strrev.c
@@ -0,0 +1,57 @@
+/*-----------------------------------------------------
+-                               __                    -
+-                             .d$$b                   -
+-                           .' TO$;\                  -
+-                          /  : TP._;                 -
+-                         / _.;  :Tb|                 -
+-                        /   /   ;j$j                 -
+-                    _.-"       d$$$$                 -
+-                  .' ..       d$$$$;                 -
+-                 /  /P'      d$$$$P. |\              -
+-                /   "      .d$$$P' |\^"l             -
+-              .'           `T$P^"""""  :             -
+-          ._.'      _.'                ;             -
+-       `-.-".-'-' ._.       _.-"    .-"              -
+-     `.-" _____  ._              .-"                 -
+-    -(.g$$$$$$$b.              .'                    -
+-      ""^^T$$$P^)            .(:                     -
+-        _/  -"  /.'         /:/;                     -
+-     ._.'-'`-'  ")/         /;/;                     -
+------------------------------------------------------*/
+
+#include <stdio.h>
+
+char	*ft_strrev(char *str)
+{
+	int	len;
+	int	count;
+	char	ascii;
+
+	len = 0;
+	count = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	len = len - 1;
+	while (count < len)
+	{
+		ascii = str[count];
+		str[count] = str[len];	
+		str[len] = ascii;
+
+		count++;
+		len--;
+	}
+	return (str);
+}
+
+int	main(void)
+{
+	char str[] = "abcdefg";
+
+	printf("%s / ", str);
+	ft_strrev(str);
+	printf("%s", str);
+	return (0);
+}
