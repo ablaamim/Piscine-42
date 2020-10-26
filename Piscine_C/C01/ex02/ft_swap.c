1319af4b55f46d1a743882bbdf374a05614e0fea
alaamimi <aku-no-hana@outlook.fr>
2020-10-27 01:27:31 +0200

    C01 progress

--- Piscine_C/C01/ex02/ft_swap.c
+++ Piscine_C/C01/ex02/ft_swap.c
@@ -19,7 +19,53 @@
 -     ._.'-'`-'  ")/         /;/;                     -
 ------------------------------------------------------*/
 
-void	ft_ft(int *nbr)
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
 {
-	*nbr = 42;
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long nbr;
+
+	nbr = nb;
+	if(nbr < 0)
+	{
+		nbr = -nbr;
+		ft_putchar('-');
+	}
+	if(nbr > 9)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+		ft_putchar(nbr + 48);
+}
+
+void	ft_swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	main(void)
+{
+	int a;
+	int b;
+	
+	a = 1337;
+	b = 42;
+	ft_swap(&a, &b);
+	ft_putnbr(a);
+	ft_putchar(10);
+	ft_putnbr(b);
+	ft_putchar(10);
+	return (EXIT_SUCCESS);
 }
