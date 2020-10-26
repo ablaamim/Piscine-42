6a6c6bdbe2f173680e8ca383264a58f22fd545aa
alaamimi <aku-no-hana@outlook.fr>
2020-10-27 01:48:01 +0200

    div_mod

---
+++ Piscine_C/C01/ex03/ft_div_mod.c
@@ -0,0 +1,70 @@
+/*-----------------------------------------------------
+-                               __                    -
+-                             .d$$b                   -
+-                           .' TO$;\                  -
+-                          /  : TP._;                 -
+-                         / _.;  :Tb|                 -
+-                        /   /   ;j$j                 -
+-                    _.-"       d$$$$                 -
+-                  .' ..       d$$$$;                 -
+-                 /  /P'      d$$$$P. |\              -
+-                /   "      .d$$$P' |\^"l             -
+-              .'           `T$P^"""""  :             -
+-          ._.'      _.'                ;             -
+-       `-.-".-'-' ._.       _.-"    .-"              -
+-     `.-" _____  ._              .-"                 -
+-    -(.g$$$$$$$b.              .'                    -
+-      ""^^T$$$P^)            .(:                     -
+-        _/  -"  /.'         /:/;                     -
+-     ._.'-'`-'  ")/         /;/;                     -
+------------------------------------------------------*/
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long nbr;
+
+	nbr = nb;
+	if(nbr < 0)
+	{
+		nbr = -nbr;
+		ft_putchar('-');
+	}
+	if(nbr > 9)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+		ft_putchar(nbr + 48);
+}
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int	main(void)
+{
+	int a;
+	int b;
+	int div;
+	int mod;
+
+	a = 10;
+	b = 3;
+	ft_div_mod(a, b, &div, &mod);
+	ft_putnbr(div);
+	ft_putchar(10);
+	ft_putnbr(mod);
+	ft_putchar(10);
+	return(EXIT_SUCCESS);
+}
