a9791b40b0441682b5cc1e79d328b0e672afe057
alaamimi <aku-no-hana@outlook.fr>
2020-10-27 05:15:54 +0200

    Update C01

--- Piscine_C/C01/ex00/ft_ft.c
+++ Piscine_C/C01/ex00/ft_ft.c
@@ -18,8 +18,48 @@
 -        _/  -"  /.'         /:/;                     -
 -     ._.'-'`-'  ")/         /;/;                     -
 ------------------------------------------------------*/
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_ft(int *nbr)
 {
 	*nbr = 42;
+}
+
+void	ft_putnbr(int nb)
+{
+	long nbr;
+
+	nbr = nb;
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		ft_putchar('-');
+	}
+	if (nbr > 9)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+		ft_putchar(nbr + 48);
+}
+
+int	main(void)
+{
+	int *nbr;
+	int n;
+
+	n = 1;
+	nbr = &n;
+	ft_putnbr(n);
+	ft_putchar(10);
+	ft_ft(nbr);
+	ft_putnbr(n);
+	return(EXIT_SUCCESS);
 }
--- Piscine_C/C01/ex01/ft_ultimate_ft.c
+++ Piscine_C/C01/ex01/ft_ultimate_ft.c
@@ -18,8 +18,65 @@
 -        _/  -"  /.'         /:/;                     -
 -     ._.'-'`-'  ")/         /;/;                     -
 ------------------------------------------------------*/
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long nbr;
+
+	nbr = nb;
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		ft_putchar('-');
+	}
+	if (nbr > 9)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+		ft_putchar(nbr + 48);
+}
 
 void	ft_ultimate_ft(int *********br)
 {
 	*********br = 42;
+}
+
+int	main(void)
+{
+	int n;
+	int *ptr;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int ********ptr8;
+	int *********ptr9;
+
+
+	n = 0;
+	ptr = &n;
+	ptr2 = &ptr;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;	
+	ft_putnbr(n);
+	ft_putchar(10);
+	ft_ultimate_ft(ptr9);
+	ft_putnbr(n);
+	return(EXIT_SUCCESS);
 }
