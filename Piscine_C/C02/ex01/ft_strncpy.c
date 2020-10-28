7520efb14a3f99aee1702250869658a37150b04a
alaamimi <aku-no-hana@outlook.fr>
2020-10-28 06:53:49 +0200

    ft_strncpy pushed

---
+++ Piscine_C/C02/ex01/ft_strncpy.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while(*str != '\0')
+	{
+		write(1, &(*str++), 1);
+	}
+}
+
+char *ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while(i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while(i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return(dest);
+}
+
+int	main(void)
+{
+	char dest[256];
+	char src[] = "12345678";
+	unsigned int n;
+
+	n = 9;
+	ft_putstr(ft_strncpy(dest, src, n));
+	ft_putchar('\n');
+	return(EXIT_SUCCESS);
+}
