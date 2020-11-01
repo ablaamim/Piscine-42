30cf60e0c187d07229167353f1b7d13b5363e89d
alaamimi <aku-no-hana@outlook.fr>
2020-11-01 02:04:48 +0200

    C04 Great exercices

---
+++ Piscine_C/C03/ex03/ft_strncat.c
@@ -0,0 +1,46 @@
+/*-----------------------------------------------------
+-                               __                    -
+-                             .d$$b                   -
+-                           .' TO$;\                  -
+-                          /  : TP._;                 -
+-                         / _.;  :Tb|                 -
+-                        /   /   ;j$j                 -
+-                    _.-"       d$$$$                 -
+-                  .' ..       d$$$$;                 -
+-                 /  /P'      d$$$$P. |\              -
+-                /   "      .d$$$P' |\^"l             -
+-              .'           `T$P^"""""  :             -
+-          ._.'      _.'                ;             -
+-       `-.-".-'-' ._.       _.-"    .-"              -
+-     `.-" _____  ._              .-"                 -
+-    -(.g$$$$$$$b.              .'                    -
+-      ""^^T$$$P^)            .(:                     -
+-        _/  -"  /.'         /:/;                     -
+-     ._.'-'`-'  ")/         /;/;                     -
+------------------------------------------------------*/
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char			*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	int				size;
+
+	size = ft_strlen(dest);
+	i = 0;
+	while (src[i] && i < nb)
+	{
+		dest[size + i] = src[i];
+		i++;
+	}
+	dest[size + i] = '\0';
+	return (dest);
+}
