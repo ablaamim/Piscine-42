14b72751dfad8c5f6f44b6c1b4761dd31740ef08
root <root@DESKTOP-8H3QRBL.localdomain>
2020-11-16 02:37:05 +0100

    CPY PASTA THAT STR

---
+++ Piscine_C/C02/ex00/fullcode.c
@@ -0,0 +1,48 @@
+/*-----------------------------------------------------
+-                               __                    -
+-                             .d$$b                   -
+-                           .' TO$;\                  -
+-                          /  : TP._;                 -
+-                         / _.;  :Tb|                 -
+-                        /   /   ;j$j                 -
+-                    _.-"       d$$$$                 -
+-                  .' ..       d$$$$;                 -
+-                 /  /P'      d$$$$P. |\              -
+-                /   "      .d$$$P' |\^"l             -
+-              .'           `T$P^"""""  :             -
+-          ._.'      _.'                ;             -
+-       `-.-".-'-' ._.       _.-"    .-"              -
+-     `.-" _____  ._              .-"                 -
+-    -(.g$$$$$$$b.              .'                    -
+-      ""^^T$$$P^)            .(:                     -
+-        _/  -"  /.'         /:/;                     -
+-     ._.'-'`-'  ")/         /;/;                     -
+------------------------------------------------------*/
+
+#include <stdlib.h>
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while(*str != '\0')
+		write(1, &(*str++), 1);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	while(*src)
+	{
+		*dest++ = *src++;
+	}
+	*dest = '\0';
+	return(dest);
+}
+
+int	main(void)
+{
+	char src[] = "testing\n";
+	char dest[256];
+	ft_strcpy(dest, src);
+	ft_putstr(dest);
+	return (EXIT_SUCCESS);
+}
