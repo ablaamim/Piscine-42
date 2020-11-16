e98b4b85413f6ded437f23e4edba563719407159
root <root@DESKTOP-8H3QRBL.localdomain>
2020-11-16 09:53:07 +0100

    HAK LFA9IRA D MOK

--- Piscine_C/C02/ex00/ft_strcpy.c
+++ Piscine_C/C02/ex00/ft_strcpy.c
@@ -5,24 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/16 02:04:22 by alaamimi          #+#    #+#             */
-/*   Updated: 2020/11/16 02:36:18 by alaamimi         ###   ########.fr       */
+/*   Created: 2020/11/16 09:39:37 by alaamimi          #+#    #+#             */
+/*   Updated: 2020/11/16 09:52:19 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putstr(char *str)
+char *ft_strcpy( char *dest, char *src)
 {
-	while(*str != '\0')
-	{
-		write(1, &(*str++), 1);
-	}
-}
-
-char	*ft_strcpy(char *dest, char *src)
-{
-	while(*src != '\0')
+	while (*src != '\0')
 	{
 		*dest++ = *src++;
 	}
 	*dest = '\0';
 	return (dest);
+}
