ae1a53eabeb1b980d27e826298ac15ffb017c1f2
alaamimi <aku-no-hana@outlook.fr>
2021-02-28 22:01:46 +0100

    ft_putnbr.c

--- Piscine_C/C00/ex07/fullcode.c
+++ Piscine_C/C00/ex07/fullcode.c
@@ -1,16 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   fullcode.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/28 21:55:49 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/02/28 21:55:51 by alaamimi         ###   ########.fr       */
+/*   Created: 2021/02/28 19:53:35 by alaamimi          #+#    #+#             */
+/*   Updated: 2021/02/28 22:01:14 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_putnbr(int nb)
 {
@@ -29,4 +35,24 @@ void	ft_putnbr(int nb)
 	}
 	if (nbr >= 0 && nbr < 10) 
 		ft_putchar(nbr + '0');
+}
+
+int	main(void)
+{
+	write(1, "-----\n", 6);
+	ft_putnbr(42);
+		ft_putchar('\n');
+	write(1, "-----\n", 6);
+	ft_putnbr(-42);
+		ft_putchar('\n');
+	write(1, "-----\n", 6);
+	ft_putnbr(0);
+		ft_putchar('\n');
+	write(1, "-----\n", 6);
+	ft_putnbr(2147483647);
+		ft_putchar('\n');
+	write(1, "-----\n", 6);
+	ft_putnbr(-2147483648);
+		ft_putchar('\n');
+	return(EXIT_SUCCESS);
 }
