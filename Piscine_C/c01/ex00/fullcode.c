47368cb8fbd2ae806bd122fd6ec9f58c5f4f7f14
alaamimi <aku-no-hana@outlook.fr>
2021-03-02 16:44:32 +0100

    ft_ft + Readme

--- Piscine_C/c01/ex00/fullcode.c
+++ Piscine_C/c01/ex00/fullcode.c
@@ -5,12 +5,54 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/02 16:32:29 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/03/02 16:33:25 by alaamimi         ###   ########.fr       */
+/*   Created: 2021/03/02 16:25:21 by alaamimi          #+#    #+#             */
+/*   Updated: 2021/03/02 16:30:25 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void	ft_ft(int *nbr)
 {
 	*nbr = 42;
+}
+
+void	ft_putnbr(int nb)
+{
+	long nbr;
+
+	nbr = nb;
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		ft_putchar('-');
+	}
+	if (nbr >= 10)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	if (nbr >= 0 && nbr < 10)
+		ft_putchar(nbr + '0');
+}
+
+int	main(void)
+{
+	int *nbr;
+	int n;
+
+	n = 1;
+	nbr = &n;
+	ft_putnbr(n);
+		ft_putchar('\n');
+	ft_ft(nbr);
+	ft_putnbr(n);
+		ft_putchar('\n');
+	return(EXIT_SUCCESS);
 }
---
+++ Piscine_C/c01/readme.md
@@ -0,0 +1,29 @@
+:information_source: Table Content :
+
+| C01 | Problems |
+|--- |--- |
+| ex00 | ft_ft |
+| ex01 | ft_ultimate_ft |
+
+## Exercise 00 : ft_ft
+
+1. :dart: Task :
+```c
+• Create a function that takes a pointer to int as a parameter, and sets the value "42"
+to that int.
+• Here’s how it should be prototyped :
+void ft_ft(int *nbr);
+```
+
+2. :dart: Function :
+```c
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+```
+
+3. :wrench: :beetle: Test && Debug :
+```c
+
+```
