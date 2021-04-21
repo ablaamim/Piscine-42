57907de93e384fce01bb9101224613df4dfdf99f
alaamimi <aku-no-hana@outlook.fr>
2021-04-21 17:14:56 +0200

    ft_strlen

--- Piscine_C/c01/ex06/fullcode.c
+++ Piscine_C/c01/ex06/fullcode.c
@@ -5,9 +5,54 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/21 17:13:44 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/04/21 17:14:28 by alaamimi         ###   ########.fr       */
+/*   Created: 2021/04/21 17:12:49 by alaamimi          #+#    #+#             */
+/*   Updated: 2021/04/21 17:13:13 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long nbr;
+
+	nbr = nb;
+	if (nbr < 0)
+	{
+		nbr = -nbr;
+		ft_putchar('-');
+	}
+	if (nbr > 9)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+		ft_putchar(nbr + 48);
+}
+
+int	ft_strlen(char *str)
+{
+	int cpt;
+
+	cpt = 0;
+	while(*str++ != '\0')
+	{
+		cpt++;
+	}
+	return(cpt);
+}
+
+int	main(void)
+{
+	char *str;
+	str = "AA123456789";
+	ft_putnbr(ft_strlen(str));
+	return(EXIT_SUCCESS);
+}
