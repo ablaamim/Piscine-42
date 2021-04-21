50e10140ba6b8e63e581b97f4b82a2faf179479b
alaamimi <aku-no-hana@outlook.fr>
2021-04-21 17:34:31 +0200

    strlen

--- Piscine_C/c01/ex06/ft_strlen.c
+++ Piscine_C/c01/ex06/ft_strlen.c
@@ -6,8 +6,20 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 17:13:44 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/04/21 17:14:28 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/04/21 17:18:09 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	
+int	ft_strlen(char *str)
+{
+	int cmt;
+	int i;
+
+	cmt = 0;
+	i = 0;
+	while(str[i] != '\0')
+	{
+		cmt++;
+	}
+	return (cmt);
+}
