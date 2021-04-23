0be8ffa39038b4a76b947297a326694b03a19f87
alaamimi <aku-no-hana@outlook.fr>
2021-04-23 02:52:34 +0200

    strcpy

--- Piscine_C/c02/ex01/ft_strncpy.c
+++ Piscine_C/c02/ex01/ft_strncpy.c
@@ -6,11 +6,11 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 02:25:13 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/04/23 02:26:51 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/04/23 02:28:56 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	while(n-- && (*dest++ = *src++));
+	while(n-- && (((*dest++ = *src++) !- '\0'));
 }
