a2c7e7a28e51622a661c620bbad492382f66b0a9
alaamimi <aku-no-hana@outlook.fr>
2021-04-25 00:06:39 +0200

    Norm V3

--- C/c00/ex01/ft_print_alphabet.c
+++ C/c00/ex01/ft_print_alphabet.c
@@ -6,11 +6,9 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 06:05:55 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/02/24 19:14:55 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/04/25 00:04:39 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
 
 void	ft_putchar(char c)
 {
@@ -19,10 +17,10 @@ void	ft_putchar(char c)
 
 void	ft_print_alphabet(void)
 {
-	char ltr;
+	char	ltr;
 
 	ltr = 97;
-	while(ltr <= 122)
+	while (ltr <= 122)
 	{
 		ft_putchar(ltr);
 		ltr++;
