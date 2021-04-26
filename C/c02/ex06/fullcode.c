3c3d8186a166e90fa729d83a5138b363e1937b72
alaamimi <aku-no-hana@outlook.fr>
2021-04-26 03:22:39 +0200

    str_is_printable

---
+++ C/c02/ex06/fullcode.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fullcode.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/26 02:46:33 by alaamimi          #+#    #+#             */
+/*   Updated: 2021/04/26 03:19:48 by alaamimi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, &(*str++),1);
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb >= 0 && nb <= 9)
+		ft_putchar(nb + 48);
+}
+
+int	ft_str_is_printable(char *str)
+{
+	if (*str == '\0')
+		return (1);
+	while (*str != '\0')
+	{
+		if (!(*str >= ' ' && *str <= '~'))
+			return (0);
+		*str++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	/*TEST 1 : */
+	char	str[] = "";
+		write(1, "-----\n", 6);
+	ft_putstr(str);
+		write(1, "\n", 1);
+		write(1, "-----\n", 6);
+	ft_str_is_printable(str);
+	ft_putnbr(ft_str_is_printable(str));
+		write(1, "\n", 1);
+		write(1, "-----\n", 6);
+	/*TEST 2 : */
+	char	str1[] = "~{}~=!!?";
+	ft_putstr(str1);
+		write(1, "\n", 1);
+		write(1, "-----\n", 6);
+	ft_str_is_printable(str1);
+	ft_putnbr(ft_str_is_printable(str1));
+		write(1, "\n", 1);
+		write(1,"-----\n", 6);
+	/*TEST 3 :*/
+	char	str2[] = " <";
+	ft_putstr(str2);
+		write(1, "\n", 1);
+		write(1, "-----\n", 6);
+	ft_str_is_printable(str2);
+	ft_putnbr(ft_str_is_printable(str2));
+	write(1, "\n", 1);
+	write(1, "-----\n", 6);
+	return (EXIT_SUCCESS);
+}
