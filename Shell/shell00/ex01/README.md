48ca28734646797c93e952a3a021d823812a6e53
alaamimi <aku-no-hana@outlook.fr>
2021-06-17 00:41:43 +0100

    description

--- Shell/shell00/ex01/README.md
+++ Shell/shell00/ex01/README.md
@@ -1,5 +1,10 @@
 #### Ok this Exercise is kind of a joke...
 
+</p>
+<p align="center">
+<img src="https://images3.memedroid.com/images/UPLOADED655/607e9077cc54b.jpeg" width="800">
+</p>
+
 Because if we read the `man cat` page we see that this command just
 *"concatenate files and print on the standard output"*. So is going to print
 whatever we put inside the target file **z**, and in this case is only needed to
--- Shell/shell00/ex01/ex01.sh
+++ Shell/shell00/ex01/ex01.sh
@@ -3,19 +3,13 @@
 #                                                         :::      ::::::::    #
 #    ex01.sh                                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+         #
+#    By: alaamimi <alaamimi@student.1337.ma>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/03/04 15:56:20 by alaamimi          #+#    #+#              #
-#    Updated: 2021/03/04 16:02:23 by alaamimi         ###   ########.fr        #
+#    Created: 2021/06/17 00:33:30 by alaamimi          #+#    #+#              #
+#    Updated: 2021/06/17 00:34:15 by alaamimi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
-# Ex00
-# Titre : Les vrais savent afficher un Z.
-# Objective : Creer un fichier z qui, si l'on fait un cat dessue, affiche "Z" suivi d'un retour a la ligne.
-# Language : Le Shell.
-# Fichiers : Echo, redirection, cat.
-
+#--------------------------------------------------------------------------------------------------
 # Echo : Afficher une chaine de char sur la sortie standard.
 # Cat : "Concatenate", Lire le contenu d'un ficher textuel.
 # redirection ">" : Permet de rediriger le resultat de la commande dans un fichier de votre choix.
