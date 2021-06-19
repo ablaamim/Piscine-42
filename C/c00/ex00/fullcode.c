c5ba91e9310ffd2d67522f4bd1635448315456e1
alaamimi <aku-no-hana@outlook.fr>
2021-06-19 23:39:06 +0100

    fullcode

--- C/c00/ex00/fullcode.c
+++ C/c00/ex00/fullcode.c
@@ -6,16 +6,16 @@
 /*   By: alaamimi <alaamimi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/19 23:14:11 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/06/19 23:14:45 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/06/19 23:38:25 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include  <unistd.h> 				/* This library is required to use the write function */
 
-void 	ft_putchar ( char c)		 / * Define the function itself, describing its body, in which we call the 'write' function
-								** to print the character received in the variable 'c' (function argument) * /
+void 	ft_putchar ( char c)		 /* Define the function itself, describing its body, in which we call the 'write' function
+									** to print the character received in the variable 'c' (function argument) */
 {
-	write ( 1 , & c, 1 );			/ * Call the 'write' function (system call) by sending it as arguments
+	write ( 1 , & c, 1 );		 /* Call the 'write' function (system call) by sending it as arguments
 								** the standard output stream, which will be used to send a character to print there.
 								** Next, we use the '&' operator so that we can take the address of the 'c' variable 
 								** and send this to the 'write' function to print the contents of the variable it points to 
@@ -23,13 +23,13 @@ void 	ft_putchar ( char c)		 / * Define the function itself, describing its body
 								** In the last argument of the 'write' function, we indicate how many bytes to the right of the extracted 
 								** we need to advance the addresses and how much data to count in order to send them to print via
 								** standard output stream using its identifier (file descriptor) that we specified
-								** in the first argument of the 'write' function * /
+								** in the first argument of the 'write' function */
 }
 
-/ * *********************************************** ********************************************* ************************************************************************/
-/ * *********************************************** ********************************************* ************************************************************************/
-**
-** The write function has the following parameters:
+/************************************************ ********************************************* ***********************************************/
+/* *********************************************** ********************************************* **********************************************/
+
+/** The write function has the following parameters:
 ** write (<stream descriptor>, <& pointer to buffer>, <number of bytes to write>):
 **
 **
@@ -62,11 +62,11 @@ void 	ft_putchar ( char c)		 / * Define the function itself, describing its body
 
 
 
-int 		main (void)				 /* Define the main function from which the program will start executing.
+int	main (void)				 /* Define the main function from which the program will start executing.
 								** 'void' in the arguments of the main function means that this function does not accept
 								** nothing in the arguments. 'int' at the beginning of the function means that the function before
 								** completion returns an integer */
-{	
+{
 	ft_putchar (':');			/* Call the 'ft_putchar' function we created for testing several times. */
 	ft_putchar (')');
 	ft_putchar ('\n');
