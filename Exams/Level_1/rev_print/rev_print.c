d20177c4e8477f7c9cb19534f5456c23707f61d0
alaamimi <aku-no-hana@outlook.fr>
2021-08-12 14:26:57 +0100

    remove

--- Exams/Level_1/rev_print/rev_print.c
+++ Exams/Level_1/rev_print/rev_print.c
@@ -1,16 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   rev_print.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaamimi <alaamimi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/10 16:22:10 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/08/10 16:23:51 by alaamimi         ###   ########.fr       */
+/*   Created: 2021/08/10 18:51:28 by alaamimi          #+#    #+#             */
+/*   Updated: 2021/08/10 18:57:14 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int index;
+
+	index = 0;
+	while (*str++)
+	{
+		index++;
+	}
+}
 
 void	ft_putchar(char c)
 {
@@ -21,7 +33,25 @@ void	ft_putstr(char *str)
 {
 	while (*str)
 	{
-		ft_putchar(str);
+		ft_putchar(*str);
 		str++;
 	}
+}
+
+int	main(void)
+{
+	char	str[] = "reverse me\n";
+	int	index;
+	int len;
+	int tmp;
+
+	index = 0;
+	len = ft_strlen(str);
+	while (index < len - 1)
+	{
+		tmp = str[index];
+		str[index] = str[len - 1];
+		str[len - 1] = tmp;
+	}
+	ft_putstr(str);
 }
