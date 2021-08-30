c61843ddfe40afe313efb9a9038c21fbb29bc905
alaamimi <aku-no-hana@outlook.fr>
2021-08-30 16:57:19 +0100

    ft_ultimate_ft

--- C/c01/ex04/ft_ultimate_div_mod.c
+++ C/c01/ex04/ft_ultimate_div_mod.c
@@ -6,17 +6,22 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 20:31:23 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/04/15 20:34:26 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/08/30 16:56:34 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	int div;
-	int mod;
+	int	div;
+	int	mod;
 
-	div = *a / *b;
-	mod = *a % *b;
-	div = *a;
-	mod = *b;
+	if (*b != 0)
+	{
+		div = *a / *b;
+		mod = *a % *b;
+		div = *a;
+		mod = *b;
+	}
 }
--- C/c01/ex04/fullcode.c
+++ C/c01/ex04/fullcode.c
@@ -6,23 +6,32 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 19:00:32 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/03/03 19:00:38 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/08/30 16:55:05 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str++, 1);
+	}
+}
+
 void	ft_putnbr(int nb)
 {
 	long nbr;
 
-	nbr = nb;
+	nbr = (long) nb;
 	if (nbr < 0)
 	{
 		nbr = -nbr;
@@ -31,34 +40,47 @@ void	ft_putnbr(int nb)
 	if (nbr > 9)
 	{
 		ft_putnbr(nbr / 10);
-		ft_putnbr(nbr % 10);
 	}
-	else
-		ft_putchar(nbr + 48);
+	ft_putchar(nbr % 10 + 48);
 }
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
 	int div;
 	int mod;
-
-	div = *a / *b;
-	mod = *a % *b;
-	*a = div;
-	*b = mod;
+	if (*b != 0)
+	{
+		div = *a / *b;
+		mod = *a % *b;
+		*a = div;
+		*b = mod;
+	}
 }
 
 int	main(void)
 {
-	int a;
-	int b;
+	int		a;
+	int		b;
+	char	str[] = "-------------------------------\n";
+	char	str0[] = "*a = ";
+	char	str1[] = "*b = ";
+	char	str2[] = "Memory adress : ";
 
 	a = 10;
 	b = 3;
+		ft_putstr(str);
 	ft_ultimate_div_mod(&a, &b);
+		ft_putstr(str0);
 	ft_putnbr(a);
-	ft_putchar(10);
+		ft_putchar('\n');
+		ft_putstr(str1);
 	ft_putnbr(b);
-	ft_putchar(10);
+		ft_putchar('\n');
+		ft_putstr(str);
+		ft_putstr(str2);
+	printf("%p\n", &a);
+		ft_putstr(str2);
+	printf("%p\n", &b);
+		ft_putstr(str2);
 	return(EXIT_SUCCESS);
 }
