34c4ce964b422d7dba2e9641e6648359a00266aa
alaamimi <aku-no-hana@outlook.fr>
2021-09-12 18:55:31 +0100

    ft_strcpy.c

--- C/c02/ex00/ft_strcpy.c
+++ C/c02/ex00/ft_strcpy.c
@@ -6,13 +6,17 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 09:39:37 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/04/25 20:08:47 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/09/12 18:30:23 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 char	*ft_strcpy( char *dest, char *src)
 {
-	while (*dest != '\0')
+	if ( dest == NULL)
+		return (NULL);
+	while (*dest)
 		*dest++ = *src++;
 	*dest = '\0';
 	return (dest);
--- C/c02/ex00/fullcode.c
+++ C/c02/ex00/fullcode.c
@@ -6,10 +6,9 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 22:18:37 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/04/25 20:10:22 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/09/12 18:55:20 by alaamimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <stdlib.h>
 #include <unistd.h>
@@ -21,7 +20,7 @@ void	ft_putchar(char c)
 
 void	ft_putstr(char *str)
 {
-	while(*str != '\0')
+	while(*str)
 	{
 		ft_putchar(*str++);
 	}
@@ -29,36 +28,35 @@ void	ft_putstr(char *str)
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	while(*src != '\0')
+	if ((dest == NULL) || (src == NULL))
+		return (NULL);
+	while (*src)
 		*dest++ = *src++;
 	*dest = '\0';
-	return(dest);
+	return (dest);
 }
 
 int	main(void)
 {
-	/*Test 1 :*/
-	char	src[] = "testing\n";
-	char	dest[] = "qq   qqqq\n";
-		write(1, "-----\n", 6);
-		write(1, "Destination string is : ", 24);
-	ft_putstr(dest);
-		write(1, "-----\n", 6);
-	ft_strcpy(dest, src);
-		write(1, "The result is : ", 16);
-	ft_putstr(dest);
-		write(1, "-----\n", 6);
-	/*Test 2 :*/
-	char	src1[] = "";
-	char	dest1[] = "42";
-		write(1, "Destination string is : ", 24);
-	ft_putstr(dest1);
-		write(1, "\n", 1);
-		write(1, "-----\n", 6);
+	char	str[] = "-------------------------\n";
+	char	str1[] = "NULL TEST! NO DISPLAY IN STDOUT.\n";
+	char	src1[] = "copy me\n";
+	char	dest1[256];
+	char	src2[] = "";
+	char	dest2[256];
+	char	src3[] = "Future is loading\n";
+	char	dest3[] = "\0";
+
 	ft_strcpy(dest1, src1);
-		write(1, "The result is : ", 16);
+		ft_putstr(str);
 	ft_putstr(dest1);
-		write(1, "\n", 1);
-		write(1, "-----\n", 6);
+		ft_putstr(str);
+	ft_strcpy(dest2, src2);
+	ft_putstr(dest2);
+		ft_putstr(str1);
+		ft_putstr(str);
+	ft_strcpy(dest3, src3);
+	ft_putstr(dest3);
+		ft_putstr(str);
 	return (EXIT_SUCCESS);
 }
