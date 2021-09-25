884527f4dcaf793b06230eaaefa228e7dc251d89
alaamimi <aku-no-hana@outlook.fr>
2021-09-25 21:37:20 +0100

    ft_str_is_alpha

--- C/c02/ex02/fullcode.c
+++ C/c02/ex02/fullcode.c
@@ -6,31 +6,12 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 02:13:51 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/04/25 20:57:21 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/09/25 21:36:22 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 #include <stdlib.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	while(*str != '\0')
-	{
-		ft_putchar(*str);
-		*str++;
-	}
-}
-
-void	ft_putnbr(int nb)
-{
-	if(nb >= 0 && nb < 10)
-		ft_putchar(nb + 48);
-}
+#include <stdio.h>
 
 int	ft_str_is_alpha(char *str)
 {
@@ -45,36 +26,16 @@ int	ft_str_is_alpha(char *str)
 
 int	main(void)
 {
-	/*TEST 1 :*/
-	char	str[] = "";
-		write(1, "-----\n", 6);
-	ft_putstr(str);
-		write(1, "\n", 1);
-	ft_str_is_alpha(str);
-		write(1, "-----\n", 6);
-		write(1, "Return : ", 9);
-	ft_putnbr(ft_str_is_alpha(str));
-		write(1, "\n", 1);
-		write(1, "-----\n", 6);
-	/*TEST 2 :*/
-	char str1[] = "123";
-	ft_putstr(str1);
-		write(1, "\n", 1);
-	ft_str_is_alpha(str1);
-		write(1, "-----\n", 6);
-		write(1, "Return : ",9);
-	ft_putnbr(ft_str_is_alpha(str1));
-		write(1, "\n", 1);
-		write(1, "-----\n", 6);
-	/*TEST 3 :*/
-	char	str2[] = "abcDEF";
-	ft_putstr(str2);
-		write(1, "\n", 1);
-		write(1, "-----\n", 6);
-	ft_str_is_alpha(str2);
-	write(1, "Return :", 9);
-	ft_putnbr(ft_str_is_alpha(str2));
-		write(1, "\n", 1);
-		write(1, "-----\n", 6);
+	char	test0[] = "01234";
+	char	test1[] = "Aaaaaa  a";
+	char	test2[] = "aBcd";
+
+	printf("---------------------------------------------\n");
+	printf("%d\n", ft_str_is_alpha(test0));
+	printf("---------------------------------------------\n");
+	printf("%d\n", ft_str_is_alpha(test1));
+	printf("---------------------------------------------\n");
+	printf("%d\n", ft_str_is_alpha(test2));
+	printf("---------------------------------------------\n");
 	return (EXIT_SUCCESS);
 }
