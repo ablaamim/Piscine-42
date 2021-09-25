a4e6e492b837dfcd70585335611481fd911497a7
alaamimi <aku-no-hana@outlook.fr>
2021-09-25 21:24:22 +0100

    strncpy

--- C/c02/ex01/ft_strncpy.c
+++ C/c02/ex01/ft_strncpy.c
@@ -6,14 +6,26 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 02:25:13 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/04/25 20:32:07 by alaamimi         ###   ########.fr       */
+/*   Updated: 2021/09/25 21:23:13 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	while (*src != '\0' && n--)
-		*dest++ = *src++;
-	*dest = '\0';
+	unsigned int i;
+
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (i < n && str[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
--- C/c02/ex01/fullcode.c
+++ C/c02/ex01/fullcode.c
@@ -6,7 +6,7 @@
 /*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 19:35:26 by alaamimi          #+#    #+#             */
-/*   Updated: 2021/09/25 21:19:09 by ablaamim         ###   ########.fr       */
+/*   Updated: 2021/09/25 21:23:40 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
+	if (dest == NULL)
+		return (NULL);
 	i = 0;
 	while (i < n && src[i])
 	{
