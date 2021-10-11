3b615fcf79b42c33a8f37708e2406eb5e6a93954
alaamimi <aku-no-hana@outlook.fr>
2021-10-11 14:50:15 +0100

    08

---
+++ C/c08/ex04/ft_stock_str.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablaamim <ablaamim@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 13:57:06 by ablaamim          #+#    #+#             */
+/*   Updated: 2021/10/11 14:01:09 by ablaamim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+
+#endif
---
+++ C/c08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablaamim <ablaamim@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 13:58:31 by ablaamim          #+#    #+#             */
+/*   Updated: 2021/10/11 14:43:45 by ablaamim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	ft_putchar(*str++);
+}
+
+void	ft_putnbr(int nb)
+{
+	long nbr;
+
+	nbr = (long) nb;
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr > 9)
+	{
+		ft_putnbr(nbr / 10);
+	}
+	ft_putchar(nbr % 10 + 48);
+}
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	dest = malloc (sizeof(src) * (ft_strlen(src) + 1));
+	if (dest == NULL)
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str	*tab;
+	int			i;
+	i = 0;
+	tab = malloc (sizeof(t_stock_str) * (ac + 1));
+	if (tab == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = ft_strdup(av[i]);
+		tab[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	tab[i].str = NULL;
+	return (tab);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str)
+	{
+		ft_putstr(par[i].str);
+		ft_putchar('\n');
+		ft_putnbr(par[i].size);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+int	main(void)
+{
+	char 	*arr_str [4] = {"line" , "one more line" , "last line"}; 
+
+	struct s_stock_str * a;
+	a = ft_strs_to_tab(3, arr_str); 
+	ft_show_tab (a);
+	return (0);
+}
